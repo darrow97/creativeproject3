89a3e544a05116dc0df226c46d0604ce8dca4d09
mverg <moplaysball@msn.com>
2020-11-06 11:39:00 -0700

    first commit

---
+++ creativeproject3/src/mock-data.js
@@ -0,0 +1,136 @@
+let mock = [
+  {
+    id: 1,
+    name: "James 'Logan' Howlett",
+    affinity: "protagonist",
+    alias: "Wolverine",
+    image: "wolverine-james.jpg",
+    pab: [
+      {"Regenerative Healing Factor"},
+      {"Superhuman Acute Senses, Stamina, Agility, and Reflexes"},
+      {"Adamantium bonded skeleton and claws"},
+      {"Psionic Resistance"},
+      {"Insulated Weather Adaptation"}
+    ]
+  },
+  {
+    id:"2",
+    name: "Anna Marie",
+    affinity: "protagonist",
+    alias: "Rogue",
+    image: "rogue.jpg",
+    pab: [
+      {"Power Absorption"}
+    ]
+  },
+  {
+    id:"3",
+    name: "Remy LeBeau",
+    affinity: "protagonist",
+    alias: "Gambit",
+    image: "gambit.jpg",
+    pab: [
+      {"Molecular Acceleration"},
+      {"Static Interference"},
+      {"Precognition"},
+      {"Total Kinetic Control"},
+      {"Enhanced Agility"}
+    ]
+  },
+  {
+    id:"4",
+    name: "Nathan Essex",
+    affinity: "antagonist",
+    alias: "Mister Sinister",
+    image: "sinister.jpg",
+    pab: [
+      {"Cellular Shapre Shifting"},
+      {"Superhuman Stamina, Speed, Durability, Strength, and Reflexes"},
+      {"Telepathy"},
+      {"Telekinesis"},
+      {"Teleportation"},
+      {"Immortality"}
+    ]
+  },
+  {
+    id:"5",
+    name: "Jean Grey",
+    affinity: "protagonist",
+    alias: "Jean Grey, Phoenix, Dark Phoenix",
+    image: "jean-grey.jpg",
+    pab: [
+      {"EXTENSIVE, SUMMARIZED TO 3 FOR SIMPLICITY"}
+      {"Telepathy"},
+      {"Telekinesis"},
+      {"Phoenix Force Avatar"}
+    ]
+  },
+  {
+    id:"6",
+    name: "Robert 'Bobby' Drake",
+    affinity: "protagonist",
+    alias: "Iceman",
+    image: "iceman.jpg",
+    pab: [
+      {"Thermokinesis"},
+      {"ThermalVision"},
+      {"Cryokinesis"},
+      {"Hydrokinesis"}
+    ]
+  },
+  {
+    id:"7",
+    name: "Katherine 'Kitty' Pryde",
+    affinity: "protagonist",
+    alias: "Shadowcat",
+    image: "kitty.jpg",
+    pab: [
+      {"Phasing and Phasing Extension"},
+      {"Air Walking"},
+      {"Electrical Disruption"},
+      {"Telepathic Resistance"}
+    ]
+  },
+  {
+    id:"8",
+    name: "Illyana Nikolievna Rasputina",
+    affinity: "hero",
+    alias: "Magik",
+    image: "magik.jpg",
+    pab: [
+      {"Stepping discs"},
+      {"Sorcery"},
+      {"Limbo control"},
+      {"Mystical Armor"},
+      {"Psionic Shields"}
+    ]
+  },
+  {
+    id:"9",
+    name: "Charles Xavier",
+    affinity: "protagonist",
+    alias: "Professor X",
+    image: "xavier.jpg",
+    pab: [
+      {"Telepathy"},
+      {"Telepathic Illusions, Cloak, and Camouflage"},
+      {"Mind Link, Blast, Control, Possession, Transferal, and Alteration"},
+      {"Psionic Shield, BLast"}
+    ]
+  },
+  {
+    id:"10",
+    name: "Max Eisenhardt",
+    affinity: "antagonist",
+    alias: "Magneto, Erik Lehnsherr",
+    image: "magneto.jpg",
+    pab: [
+      {"Magnetokinesis"},
+      {"Electomagnetic Spectrum Manipulation"},
+      {"Astral Projection"},
+      {"Telepathic Resistance"}
+    ]
+  }
+]
+
+export default mock;
